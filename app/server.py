a25d67c67ab773132e3c3cfadb5e555d74d2b81a
Your Name <you@example.com>
2018-09-09 12:59:56 +0900

    Add no-cache header

--- app/server.py
+++ app/server.py
@@ -22,6 +22,14 @@ def bad():
     return render_template('bad.html', name=name)
 
 
+@app.after_request
+def set_response_headers(response):
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
+
+
 @app.route('/json')
 def json():
     name = "Hey, B.A.D."
