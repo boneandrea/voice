dc6ce5cfb5cdb1e79c1fc60aa0eeec6a568af1ef
Your Name <you@example.com>
2018-09-09 12:28:37 +0900

    configure template

--- app/server.py
+++ app/server.py
@@ -1,15 +1,22 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     name = "Hello World."
-    return name
+    return render_template('index.html', name=name)
 
 @app.route('/good')
 def good():
     name = "Good"
     return name
+
+@app.route('/bad')
+def bad():
+    name = "Hey, B.A.D."
+    return render_template('bad.html', name=name)
 
 ## おまじない
 if __name__ == "__main__":
